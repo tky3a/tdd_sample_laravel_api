dbf5b3dfccc62f12d385733f2a8e2976da71949c
tky <takayamac@icloud.com>
2022-08-06 23:21:32 +0900

    api/reports/{report_id}にGETメソッドでアクセスできる実装とテスト

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::put('customers/{customer_id}', function() {});
 Route::delete('customers/{customer_id}', function() {});
 Route::get('reports', function() {});
 Route::post('reports', function() {});
+Route::get('reports/{report_id}', function() {});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -61,4 +61,11 @@ class ReportTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_api_reports_report_idにGETメソッドでアクセスできる()
+    {
+        $response = $this->get('api/reports/1');
+
+        $response->assertStatus(200);
+    }
 }
